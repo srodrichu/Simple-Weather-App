a230220cf5d9bb34add1719cb57bba584d1b04a8
srodrichu <sebastianawrchu@gmail.com>
2021-10-15 12:07:00 +0100

    Updates

---
+++ precache-manifest.dd2d48168e63ccacb9478cd78dd735c0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc43d2c1de7c8dd8e3a5386b7ac9938",
+    "url": "/Simple-Weather-App/index.html"
+  },
+  {
+    "revision": "9134845636eafd0ea32c",
+    "url": "/Simple-Weather-App/static/css/2.a1460f76.chunk.css"
+  },
+  {
+    "revision": "a16f5182339e04488ce5",
+    "url": "/Simple-Weather-App/static/css/main.b4cbef20.chunk.css"
+  },
+  {
+    "revision": "9134845636eafd0ea32c",
+    "url": "/Simple-Weather-App/static/js/2.92019e7f.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/Simple-Weather-App/static/js/2.92019e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bd48d5aa6e43698c59e",
+    "url": "/Simple-Weather-App/static/js/3.a1a9ea74.chunk.js"
+  },
+  {
+    "revision": "a16f5182339e04488ce5",
+    "url": "/Simple-Weather-App/static/js/main.b5c9f852.chunk.js"
+  },
+  {
+    "revision": "4e81f49260108a713f70",
+    "url": "/Simple-Weather-App/static/js/runtime-main.5f4eae5c.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/Simple-Weather-App/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/Simple-Weather-App/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/Simple-Weather-App/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/Simple-Weather-App/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/Simple-Weather-App/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
